74e7868ef61d5cf3c8b7951aa64b5aca48debdc9
Oleg Andreev <oleganza@gmail.com>
2008-08-03 04:08:33 +0400

    added views/pretty_finder_layer (not complete yet)

---
+++ lib/strokedb-core/views/pretty_finder_layer.rb
@@ -0,0 +1,13 @@
+module StrokeDB
+  module Core
+    module Views
+      module PrettyFinderLayer
+
+        def find(start_key, end_key, limit, offset, reverse, with_keys)
+          
+        end
+        
+      end
+    end
+  end
+end
