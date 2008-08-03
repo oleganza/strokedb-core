d1a19c6f205cd1efb71a77c55d78e527a2b6930f
Oleg Andreev <oleganza@gmail.com>
2008-08-04 00:58:14 +0400

    added views/default_value_codec

---
+++ lib/strokedb-core/views/default_value_codec.rb
@@ -0,0 +1,18 @@
+module StrokeDB
+  module Core
+    module Views
+      # Default codec just passes value by.
+      module DefaultValueCodec
+
+        def encode_value(v)
+          v
+        end
+        
+        def decode_value(ev)
+          ev
+        end
+
+      end
+    end
+  end
+end
