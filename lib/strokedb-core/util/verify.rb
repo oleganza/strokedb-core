af8d871bf7f003461a88e7b701a7cdc1f580191f
Oleg Andreev <oleganza@gmail.com>
2008-08-08 17:27:52 +0400

    refactored util/verify.rb

--- lib/strokedb-core/util/verify.rb
+++ lib/strokedb-core/util/verify.rb
@@ -1,34 +1,32 @@
 module StrokeDB
   module Core
     module Util
-      def self.verify(*args, &blk)
-        m = self
-        (@__verify_proxy ||= Class.new{include m; def initialize(*args, &blk); @__verify_skip_level = 2; super; end}.new).verify(*args, &blk)
-      end
       def verify(*args, &blk)
         if block_given? # test for exception
           begin
             r = yield(*args)
           rescue Exception => e
-            raise VerifyFailed.new(e, @__verify_skip_level), "Verification failed!"
+            raise VerifyFailed.new(e), "Verification failed!"
           else
-            r or raise VerifyFailed.new(nil, @__verify_skip_level), "Verification failed!"
+            r or raise VerifyFailed.new(nil), "Verification failed!"
           end
         else
           if args.size == 1
-            args.first or raise VerifyFailed.new(nil, @__verify_skip_level), "Verification failed!"
+            args.first or raise VerifyFailed.new(nil), "Verification failed!"
           else
             args.each_with_index do |a, i|
-              a or raise VerifyFailed.new(nil, @__verify_skip_level), "Verification failed at the #{i+1} argument!"
+              a or raise VerifyFailed.new(nil), "Verification failed at the #{i+1} argument!"
             end
             true
           end
         end
       end
+      
+      class <<self; self; end.send(:define_method, :verify, instance_method(:verify))
+
       class VerifyFailed < Exception
-        def initialize(exception = nil, level = 1)
+        def initialize(exception = nil)
           @exception = exception
-          @level = level
         end
         def message
           return super unless @exception
@@ -36,7 +34,7 @@ module StrokeDB
         end
         def backtrace
           return super unless @exception
-          @exception.backtrace[(@level || 1)..-1]
+          @exception.backtrace[1..-1]
         end
       end
     end
