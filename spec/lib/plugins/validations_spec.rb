d41201d7e7e14a77b07abb2fc1137e1aee89413d
Oleg Andreev <oleganza@gmail.com>
2008-08-11 07:26:49 +0400

    draft spec for validations

---
+++ spec/lib/plugins/validations_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
+
+describe "Validations" do
+  
+  it "should be inheritable when defined in base class"
+  it "should be inheritable when defined in a module"
+  it "should have clean well-defined interface"
+  it "should implement basic validations"
+  
+end  
