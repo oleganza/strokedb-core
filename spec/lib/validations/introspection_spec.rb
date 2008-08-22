224a4b4eceeab7a133134b88e84646883fc5a47a
Oleg Andreev <oleganza@gmail.com>
2008-08-22 17:30:48 +0400

    tiny spec title fix

--- spec/lib/validations/introspection_spec.rb
+++ spec/lib/validations/introspection_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 
-describe "Validations" do
+describe "Validations[introspection]" do
   
   before :each do
     @contacts_validations = contacts_validations = Module.new do
