ea2a882fdff9678e3c7cab1590c3c5cd3e90b685
Oleg Andreev <oleganza@gmail.com>
2008-08-22 20:52:58 +0400

    simple slot hooks

--- lib/strokedb/stroke_objects/slot_hooks.rb
+++ lib/strokedb/stroke_objects/slot_hooks.rb
@@ -5,28 +5,58 @@ module StrokeDB
         mod.extend(ClassMethods)
       end
       
-      def initialize(*args)
-        super
-        @hooks ||= {}
+      def slot_hooks
+        @slot_hooks ||= self.class.slot_hooks
       end
       
       def [](slotname)
-        if hook = @get_hooks[slotname]
-          return hook.get { super }
-        end
-        super
+        return super unless hook = slot_hooks[slotname]
+        return hook.get(self, slotname) { super }
       end
       
       def []=(slotname, value)
-        if hook = @set_hooks[slotname]
-          return hook.set(value) { super }
-        end
-        super
+        return super unless hook = slot_hooks[slotname]
+        return hook.set(self, slotname, value) { super }
       end
       
       module ClassMethods
-        # def set_hook
+        attr_accessor :slot_hooks
+        def slot_hooks
+          @slot_hooks ||= {}
+        end
+        
+        # Example:
+        #
+        # class Person < Hash
+        #   include SlotHooks
+        #
+        #   slot_hook :name do
+        #     def get(doc, slotname)
+        #       puts "Accessing slot #{slotname}"
+        #       super
+        #     end
+        #     def set(doc, slotname, value)
+        #       puts "Writing to slot #{slotname}"
+        #       super
+        #     end
+        #   end
         # end
+        #
+        def slot_hook(slotname, mod = nil, &blk)
+          slot_hooks[slotname] ||= SlotHook.new
+          slot_hooks[slotname].extend(mod || Module.new(&blk))
+        end
+      end
+      
+      # Default slot hook: noop.
+      class SlotHook        
+        def get(doc, slotname)
+          yield # returns super
+        end
+        
+        def set(doc, slotname, value)
+          yield # returns super
+        end
       end
     end
   end
