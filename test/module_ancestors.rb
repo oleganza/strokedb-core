35389b8d9246173e221933228f4575f5c0165af0
Oleg Andreev <oleganza@gmail.com>
2008-08-23 18:53:16 +0400

    added test for Module#all_ancestors

--- test/module_ancestors.rb
+++ test/module_ancestors.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/env ruby
 
+class Module
+  def all_ancestors(collected = [])
+    collected << self
+    (ancestors - collected).inject(collected) do |c, a|
+      (c | a.all_ancestors(c))
+    end
+  end
+end
+
 module A1; end
 module A2; end
 
@@ -20,4 +29,9 @@ module B1
 end
 
 # Where's module A1?
-p C.ancestors # => [C, B2, A2, B1, Object, Kernel]
+
+p([A1, A2, B1, B2].map{|m| m.all_ancestors == m.ancestors })
+
+puts(C.all_ancestors - C.ancestors == [ A1 ])
+puts(C.all_ancestors == [C, B2, A2, B1, A1, Object, Kernel])
+puts(C.ancestors == [C, B2, A2, B1, Object, Kernel])
