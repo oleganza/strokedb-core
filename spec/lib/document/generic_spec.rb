e5da7f132143671b9ff8ff2fbe72fdd2678f73a3
Oleg Andreev <oleganza@gmail.com>
2008-08-25 17:03:55 +0200

    updated document/generic_spec

--- spec/lib/document/generic_spec.rb
+++ spec/lib/document/generic_spec.rb
@@ -5,9 +5,17 @@ describe "document with metas", :shared => true do
   it { @doc[:uuid].should_not be_nil }
   it { @doc[:metas].should be_kind_of(Array) }
   it { @doc[:metas].should_not be_empty }
-  it "for each meta should respond kind_of?(meta)" do
+  it "for each meta should respond positively to kind_of?, is_a?" do
     @doc[:metas].each do |meta|
-      @doc.should be_kind_of(meta)
+      @doc.is_a?(meta).should == true
+      @doc.kind_of?(meta).should == true
+      meta.should === meta
+      meta.should === @doc
+    end
+  end
+  it "should be a positive target against meta.===" do
+    @doc[:metas].each do |meta|
+      meta.should === @doc # doc
     end
   end
   
@@ -15,9 +23,22 @@ end
 
 describe "meta", :shared => true do
   
-  it { @doc[:name].should_not be_nil }
-  it { @doc[:nsurl].should_not be_nil }
+  it { @doc.should equal(@doc) }
+  it { @doc.should eql(@doc) }
+  it { @doc.should == @doc }
+  it { @doc.should === @doc }
   it { @doc[:uuid].should_not be_nil }
+  it "should have both name and nsurl or none at all" do
+    if @doc[:name] || @doc[:nsurl]
+      @doc[:name].should_not be_nil
+      @doc[:nsurl].should_not be_nil
+    end
+  end
+  it "should have UUID calculated from name+nsurl if name's given" do
+    if @doc[:name]
+      @doc[:uuid].should == Util.sha1_uuid(@doc[:nsurl] + "/" + @doc[:name])
+    end
+  end
 
   it_should_behave_like "document with metas"
   
