5be80c0f187f0bb31e569db2a48edc12aa75b55a
Oleg Andreev <oleganza@gmail.com>
2008-08-25 03:41:48 +0300

    simplified Declarations code

--- lib/strokedb/util/declarations.rb
+++ lib/strokedb/util/declarations.rb
@@ -31,22 +31,67 @@ module StrokeDB
   # We choose to be smartass (option 2)
   #
   module Declarations
-    def self.included(dsl_module)
+    
+    def include(mod)
+      invalidate_decl_cache!
+      mod.extend(Declarations)
+      mod.register_cache(self) # self must be invalidated when mod's ancestors are modified
       super
-      # 1. Install hooks on #included and #inherited to
-      #    provide declarations 
-      # 2. Install other hooks for proper cache invalidation.
-      #    Cache contains a list of inherited data.
-      dsl_module.send(:include, API)
     end
-        
+    
+    # Util.sha1_uuid("StrokeDB::Declarations::API::NOTHING").upcase.gsub(/-/,'_')
+    #    #==>"CA1AF719_3EC3_56BB_A98A_C67AD40A5266"
+    CA1AF719_3EC3_56BB_A98A_C67AD40A5266 = Object.new.freeze
+  
+    def local_declarations(name, default = CA1AF719_3EC3_56BB_A98A_C67AD40A5266, &blk)
+      decls = (@local_declarations ||= Hash.new(CA1AF719_3EC3_56BB_A98A_C67AD40A5266))
+      decls[name] = default if decls[name].equal?(CA1AF719_3EC3_56BB_A98A_C67AD40A5266)
+      return local_declarations_set(name, yield(decls[name])) if block_given?
+      decls[name]
+    end
+  
+    def local_declarations_set(name, value)
+      decls = (@local_declarations ||= {})
+      decls[name] = value
+      invalidate_decl_cache!
+      value
+    end
+  
+    def inherited_declarations(name)
+      decls = @inherited_declarations
+      decls and data = decls[name] and return data
+      @inherited_declarations ||= {}
+      inherited_data = absolutely_all_ancestors.inject([]) do |memo, ancestor|
+        ancestor.extend(Declarations)
+        ancestor.register_cache(self)
+        local_decls = ancestor.local_declarations(name)
+        memo << local_decls unless local_decls.equal?(CA1AF719_3EC3_56BB_A98A_C67AD40A5266)
+        memo
+      end
+      @inherited_declarations[name] = yield(inherited_data)
+    end
+    
+    def register_cache(mod)
+      @decl_cached_objects ||= []
+      @decl_cached_objects |= [mod]
+    end
+    
+    # It is rude to invaidate all possible DSLs when only one 
+    # DSL inheritance chain is modified, but ve-ery effective.
+    def invalidate_decl_cache!
+      return unless @decl_cached_objects
+      @decl_cached_objects.each do |mod|
+        mod.instance_variable_set(:@inherited_declarations, nil)
+      end
+    end
+    
     module ModuleMethods
       # Search ancestors for the missing method
       def method_missing(meth, *args, &blk)
         # 0. If this is a DSL and Declarations were included lazily,
         #    module may not have these methods. Extend it with them
-        if Declarations::API.instance_methods.include?(meth.to_s) and !is_a?(Declarations::API)
-          extend(Declarations::API)
+        if Declarations.instance_methods.include?(meth.to_s) and !is_a?(Declarations)
+          extend(Declarations)
           return send(meth, *args, &blk)
         end
         
@@ -68,64 +113,7 @@ module StrokeDB
         end
         super
       end
-    end
-    
-    module API
-      
-      def include(mod)
-        invalidate_decl_cache!
-        mod.extend(API)
-        mod.register_cache(self) # self must be invalidated when mod's ancestors are modified
-        super
-      end
-      
-      # Util.sha1_uuid("StrokeDB::Declarations::API::NOTHING").upcase.gsub(/-/,'_')
-      #    #==>"CA1AF719_3EC3_56BB_A98A_C67AD40A5266"
-      CA1AF719_3EC3_56BB_A98A_C67AD40A5266 = Object.new.freeze
-    
-      def local_declarations(name, default = CA1AF719_3EC3_56BB_A98A_C67AD40A5266, &blk)
-        decls = (@local_declarations ||= Hash.new(CA1AF719_3EC3_56BB_A98A_C67AD40A5266))
-        decls[name] = default if decls[name].equal?(CA1AF719_3EC3_56BB_A98A_C67AD40A5266)
-        return local_declarations_set(name, yield(decls[name])) if block_given?
-        decls[name]
-      end
-    
-      def local_declarations_set(name, value)
-        decls = (@local_declarations ||= {})
-        decls[name] = value
-        invalidate_decl_cache!
-        value
-      end
-    
-      def inherited_declarations(name)
-        decls = @inherited_declarations
-        decls and data = decls[name] and return data
-        @inherited_declarations ||= {}
-        inherited_data = absolutely_all_ancestors.inject([]) do |memo, ancestor|
-          ancestor.extend(API)
-          ancestor.register_cache(self)
-          local_decls = ancestor.local_declarations(name)
-          memo << local_decls unless local_decls.equal?(CA1AF719_3EC3_56BB_A98A_C67AD40A5266)
-          memo
-        end
-        @inherited_declarations[name] = yield(inherited_data)
-      end
-      
-      def register_cache(mod)
-        @decl_cached_objects ||= []
-        @decl_cached_objects |= [mod]
-      end
-      
-      # It is rude to invaidate all possible DSLs when only one 
-      # DSL inheritance chain is modified, but ve-ery effective.
-      def invalidate_decl_cache!
-        return unless @decl_cached_objects
-        @decl_cached_objects.each do |mod|
-          mod.instance_variable_set(:@inherited_declarations, nil)
-        end
-      end
-
-    end # API
+    end # ModuleMethods
   end # Declarations
 end # StrokeDB
 
