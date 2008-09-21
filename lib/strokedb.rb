caaecc5aedff89a456b02a5f628786e169dafb22
Oleg Andreev <oleganza@gmail.com>
2008-09-21 14:56:25 +0400

    user-friendly submodules updater

--- lib/strokedb.rb
+++ lib/strokedb.rb
@@ -4,9 +4,19 @@ require 'fileutils'
 # These libs are not published as gems yet.
 # git submodule init && git submodule update
 ($:.unshift *(Dir[ File.join( File.dirname(__FILE__), '..', 'vendor', '**', 'lib' ) ].to_a.map {|f| File.expand_path(f) }) ).uniq!
-require 'extlib'
-require 'tokyocabinet-wrapper'
-require 'declarations'
+begin
+  libs = %w[extlib tokyocabinet-wrapper declarations]
+  libs.each do |lib|
+    require lib
+  end
+rescue LoadError => e
+  puts "You need submodules #{libs.join(', ')} to be updated. Update now? [y/n]"
+  if gets.strip =~ /^y/
+    system("git submodule init && git submodule update")
+    retry
+  end
+  raise
+end
 
 require 'strokedb/version'
 require 'strokedb/constants'
